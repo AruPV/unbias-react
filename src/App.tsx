83529db2c3c871e6d57f4e7bb1acd93fcf0a846d
Aru Poleo Vargas <arupoleo@gmail.com>
2024-08-13 15:09:55 -0400

    Added shadcn and react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
 import {Input} from "@/components/ui/input"
-import './App.css'
 import { ThemeProvider } from './components/theme-provider'
 import { ModeToggle } from './components/ui/mode-toggle'
+import Header from './components/ui/header'
+import { Button } from './components/ui/button'
 
 function App() {
 
   return (
 		<ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-			<ModeToggle></ModeToggle>
-			<Input type='url' placeholder='Article URL'/>
+      <Header/>
+      <div className="flex flex-row justify-center">
+        <div className="">
+          <ModeToggle/>
+          <div className='flex flex-row items-center'>
+            <Input type='url' placeholder='Article URL' className='my-2'/>
+            <Button variant={'default'}> Fetch </Button>
+          </div>
+        </div>
+      </div>
 		</ThemeProvider>
   )
 }
---
+++ src/components/ui/header.tsx
@@ -0,0 +1,9 @@
+function Header(){
+  return(
+    <div className="bg-background border">
+      I am a header
+    </div>
+  )
+}
+
+export default Header
